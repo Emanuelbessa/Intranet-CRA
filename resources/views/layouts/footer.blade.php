c64043633793096a96543047f0674bab582759d7
Emanuel Bessa <41740085+Emanuelbessa@users.noreply.github.com>
2020-03-27 00:51:21 -0300

    Atualização do Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,9 @@
 <!-- /.content-wrapper -->
-<footer class="main-footer">
+<footer class="main-footer text-center">
     <div class="pull-right hidden-xs">
         <b>Versão</b> 1.0
     </div>
-    <strong>Copyright &copy; {{ date('Y') }} <a href="{{ env('APP_URL') }}">{{ env('APP_NAME') }}</a>.</strong> Todos os
+    <strong>Copyright &copy; {{ date('Y') }} <a href="{{ env('APP_URL') }}">Intranet CRA-BA</a>.</strong> Todos os
     Direitos Reservados.
+    <p>CRA-BA - Equipe de Desenvolvimento 2020</p>
 </footer>
